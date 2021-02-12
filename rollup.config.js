381a3abd1c7ffb68f6e8323613e5e871a5a6237f
Alex Veenendaal <alex@opendoorgonorth.com>
2021-02-12 06:55:45 +0000

    tidy up and publish 0.2.0

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,24 @@
 import typescript from 'rollup-plugin-typescript2';
 
+const tsconfigOverride = {
+  compilerOptions: { module: "es2015" }
+};
 
 const config = {
-    preserveModules: true, // or `false` to bundle as a single file
-    input: ['src/index.ts'],
-    output: [{ dir: 'dist', format: 'esm', esModule:true, entryFileNames: '[name].mjs' }],
-    external: ['jsonpointer'],
-    plugins: [typescript({ tsconfig: './tsconfig.rollup.json' })],
+  preserveModules: true, // or `false` to bundle as a single file
+  input: ['src/index.ts'],
+  output: [{ dir: 'dist', format: 'esm', esModule: true, entryFileNames: '[name].mjs' }],
+  external: ['jsonpointer'],
+  plugins: [typescript({ tsconfig: './tsconfig.json', tsconfigOverride })],
 };
 
 export default [
   {
-      ...config,
-      output: [{ dir: 'dist', format: 'esm', esModule:true, entryFileNames: '[name].mjs' }]
+    ...config,
+    output: [{ dir: 'dist', format: 'esm', esModule: true, entryFileNames: '[name].mjs' }]
   },
   {
     ...config,
-    output: [{ dir: 'dist', format: 'cjs', esModule:false, entryFileNames: '[name].js' }]
+    output: [{ dir: 'dist', format: 'cjs', esModule: false, entryFileNames: '[name].js' }]
   }
 ];
